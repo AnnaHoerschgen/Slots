f2b9ae7a432620259387aa9f63022405ab88f93b
Anna Hoerschgen <ah0959247@otc.edu>
2025-09-03 13:01:15 -0500

    Code all debugged!

--- slot_game.php
+++ slot_game.php
@@ -8,24 +8,23 @@
     echo("Running Chance Slots Simulation Version 7.77...\n"); // Initial Echo Statement
 
     // Main Game
-    while ($spins < 20 || $total_winnings < 500) {
-        $result = ""; // Blank result
-        for ($i = 0; $i <= 3; $i++) {
-            $result += array_rand($symbols);
+    while ($spins < 20 && $total_winnings < 500) {
+        $result = ["","",""]; // Blank result
+        for ($i = 0; $i < 3; $i++) {
+            $result[$i] = $symbols[array_rand($symbols)];
         } // end of for loop
 
-        $payout = 0; // current spin payout
-        match($result) {
-            'AAA', 'BBB', 'CCC' => $payout = 100, // Three identical symbols
-            'AAB', 'ABA', 'BAA', 'ABB', 'BBA', 'BAB', 'BCC', 'CBC', 'CCB', 'ACC', 'CAC', 'CCA' => $payout = 50, // Two identical symbols
-            default => $payout = 0 // Default case
+        $payout = match("{$result[0]}{$result[1]}{$result[2]}") {
+            "AAA", "BBB", "CCC" => 100, // Three identical symbols
+            "AAB", "ABA", "BAA", "ABB", "BBA", "BAB", "BCC", "CBC", "CCB", "ACC", "CAC", "CCA" => 50, // Two identical symbols
+            default => 0 // Default case
         }; // end match
 
-        $total_results[$spins] = "Spin {$spins}: {$result}, Payoff {$payout}.\n"; // Log spin result
+        $total_results[$spins] = "Spin {$spins}: {$result[0]}{$result[1]}{$result[2]}, Payoff {$payout}.\n"; // Log spin result
         $total_winnings += $payout; // add payout to total winnings
 
         $spins += 1;
-        
+
     } // end of while loop
 
     echo("Simulation finished, echoing results shortly.\n\n"); // Checkpoint echo statement
